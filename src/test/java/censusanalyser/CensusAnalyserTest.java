19d8a758cdabcc2ad1e0ac1132f0fbd742f68287
mahendrakundare <mr.kundare@gmail.com>
2019-12-09 11:49:42 +0530

    Added Test Code to load State CSV File

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -8,6 +8,8 @@ public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INDIAN_STATE_CODE="./src/test/resources/IndiaStateCode.csv";
+
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -28,5 +30,16 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
+    }
+
+    @Test
+    public void givenIndianStateCSV_ShouldReturnExactCount() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        try {
+            int numberOfStateCode = 37;
+            numberOfStateCode = censusAnalyser.loadIndianStateCode(INDIAN_STATE_CODE);
+            Assert.assertEquals(37,numberOfStateCode);
+        } catch (CensusAnalyserException e) { }
+
     }
 }
