9b42bccfe164d1b1b417df18cb794551e62192a3
mahendrakundare <mr.kundare@gmail.com>
2019-12-09 10:59:19 +0530

    Refactored code to close the file

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -12,13 +12,12 @@ import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            Iterator<IndiaCensusCSV>censusCSVIterator = csvToBean.iterator();
             Iterable<IndiaCensusCSV> csvIterable=() -> censusCSVIterator;
             int  numOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
             return numOfEateries;
