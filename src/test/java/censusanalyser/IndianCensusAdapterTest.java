0a9f8977da94903655e45454e0189dbeb63e16c7
mahendrakundare <mr.kundare@gmail.com>
2019-12-16 09:22:10 +0530

    Added Test Cases for US CensusAdapter and IndianCensus Adapter

--- src/test/java/censusanalyser/IndianCensusAdapterTest.java
+++ src/test/java/censusanalyser/IndianCensusAdapterTest.java
@@ -2,6 +2,8 @@ package censusanalyser;
 import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import java.util.Map;
 
 public class IndianCensusAdapterTest {
@@ -10,7 +12,8 @@ public class IndianCensusAdapterTest {
     private static final String INDIAN_STATE_CODE = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_FILE_TYPE = "./src/test/resources/IndianStateCensus.txt";
     private static final String US_CENSUS_CSV_FILE = "./src/test/resources/USCensusData.csv";
-
+    private static final String INDIA_WRONG_DELIMITER_CSV="./src/test/resources/IndiaStateCensusWrongDelimiter.csv";
+    private static final String INDIA_CENSUS_CSV_FILE_WITH_INVALID_HEADER="./src/test/resources/IndiaStateCensusDataInvalidHeader.csv";
     @Test
     public void givenIndianCensusCSVDataFromAdapterShouldReturnRecords() {
         try {
@@ -30,5 +33,47 @@ public class IndianCensusAdapterTest {
             CensusDAO[] censusDAOS = new Gson().fromJson(sortedData,CensusDAO[].class);
             Assert.assertEquals("",censusDAOS[0].state);
         } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenIndianCensusCSVWrongFile_ShouldThrowException() {
+        try {
+            USCensusAdapter usCensusAdapter = new USCensusAdapter();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            usCensusAdapter.loadCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFileWithWrongExtension_ShouldThrowException() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(WRONG_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFileWithDelimiterProblem_ShouldThrowException() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(INDIA_WRONG_DELIMITER_CSV);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFileWithIncorrectHeader_ShouldThrowException() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(INDIA_CENSUS_CSV_FILE_WITH_INVALID_HEADER);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM,e.type);
+        }
     }
 }
--- src/test/java/censusanalyser/USCensusAdapterTest.java
+++ src/test/java/censusanalyser/USCensusAdapterTest.java
@@ -1,6 +1,8 @@
 package censusanalyser;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import java.util.Map;
 
 public class USCensusAdapterTest {
@@ -9,6 +11,10 @@ public class USCensusAdapterTest {
     private static final String INDIAN_STATE_CODE = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_FILE_TYPE = "./src/test/resources/IndianStateCensus.txt";
     private static final String US_CENSUS_CSV_FILE = "./src/test/resources/USCensusData.csv";
+    private static final String US_WRONG_CSV_FILE="./src/test/resources/USCensusData102.csv";
+    private static final String US_WRONG_CSV_EXT="./src/test/resources/USCensusData.txt";
+    private static final String US_WRONG_DELIMITER_CSV="./src/test/resources/USCensusDataDelimiter.csv";
+    private static final String US_CENSUS_CSV_FILE_WITH_INVALID_HEADER="./src/test/resources/USCensusDataInvalidHeader.csv";
 
     @Test
     public void givenUSCensusCSVDataFromAdapterShouldReturnRecords() {
@@ -16,8 +22,49 @@ public class USCensusAdapterTest {
             CensusAdapter censusAdapter = new USCensusAdapter();
             Map<String, CensusDAO> numOfRecords= censusAdapter.loadCensusData(US_CENSUS_CSV_FILE);
             Assert.assertEquals(51, numOfRecords.size());
+        } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenUSCensusCSVWrongFile_ShouldThrowException() {
+        try {
+            USCensusAdapter usCensusAdapter = new USCensusAdapter();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            usCensusAdapter.loadCensusData(US_WRONG_CSV_FILE);
         } catch (CensusAnalyserException e) {
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM, e.type);
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCensusCSVFileWithWrongExtension_ShouldThrowException() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(US_WRONG_CSV_EXT);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCensusCSVFileWithDelimiterProblem_ShouldThrowException() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(US_WRONG_DELIMITER_CSV);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCensusCSVFileWithIncorrectHeader_ShouldThrowException() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(US_CENSUS_CSV_FILE_WITH_INVALID_HEADER);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DELIMITER_OR_HEADER_PROBLEM,e.type);
         }
     }
 }
+
